adb37c189d78d8d30ef0420bcf1466aaea042083
oyas <poteto83@gmail.com>
2013-07-07 01:53:45 +0900

    三角形の内部判定追加

---
+++ kika.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+bool checkTRIANGLE(int pos[3][2], int target[2]){
+	int x[3] = { pos[1][0] - pos[0][0],
+			pos[2][0] - pos[1][0],
+			pos[0][0] - pos[2][0] };
+	int y[3] = { pos[1][1] - pos[0][1],
+			pos[2][1] - pos[1][1],
+			pos[0][1] - pos[2][1] };
+	int tx[3] = { target[0] - pos[0][0],
+			target[0] - pos[1][0],
+			target[0] - pos[2][0] };
+	int ty[3] = { target[1] - pos[0][1],
+			target[1] - pos[1][1],
+			target[1] - pos[2][1] };
+	
+	for(int i=0; i<3; i++)
+		if( x[i]*ty[i] - y[i]*tx[i] > 0 )
+			return false;
+
+	return true;
+}
+
+
+//動作確認
+int main(){
+	int pos[3][2], target[2];
+	for(int i=0; i<3; i++)
+		scanf("%d %d", pos[i], pos[i]+1);
+	printf("ターゲット> ");
+	scanf("%d %d", target, target+1);
+
+	printf("結果：　%s\n", checkTRIANGLE(pos, target)?"TRUE":"FALSE");
+	
+	return 0;
+}
