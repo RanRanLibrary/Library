ac2fb506e7577de764aaf8e9bb5989f965a5cb5d
oyas <poteto83@gmail.com>
2013-07-08 14:53:19 +0900

    三角形の内部判定の改善

--- kika.cpp
+++ kika.cpp
@@ -22,6 +22,22 @@ bool checkTRIANGLE(int pos[3][2], int target[2]){
 	return true;
 }
 
+//三角形はどんな順でもOK。辺上はFALSEを返す。(三角形が成り立ってないときにミスるの防止)
+bool checkTRIANGLE2(int pos[3][2], int target[2]){
+	
+	for(int i=0; i<3; i++){
+		int x = pos[(i+1)%3][0] - pos[i][0],
+			y = pos[(i+1)%3][1] - pos[i][1],
+			ox = pos[(i+2)%3][0] - pos[i][0],
+			oy = pos[(i+2)%3][1] - pos[i][1],
+			tx = target[0] - pos[i][0],
+			ty = target[1] - pos[i][1];
+		if( (x*ty - y*tx) * (x*oy - y*ox) <= 0 )
+			return false;
+	}
+
+	return true;
+}
 
 //動作確認
 int main(){
@@ -32,6 +48,7 @@ int main(){
 	scanf("%d %d", target, target+1);
 
 	printf("結果：　%s\n", checkTRIANGLE(pos, target)?"TRUE":"FALSE");
+	printf("結果2：　%s\n", checkTRIANGLE2(pos, target)?"TRUE":"FALSE");
 	
 	return 0;
 }
