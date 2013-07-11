94026dec1cd9d152bbb805f2b6527b55e7b05252
nvip <nvip62@gmail.com>
2013-07-11 20:16:14 +0900

    Create extGCD.cpp
    nx+my=gcd(n,m)となる整数x,yを求める。
    mを法としたnの逆元がxとなる。
    
    gcd(n,m)はnとmの最大公約数

---
+++ extGCD.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+
+
+using namespace std;
+
+long long gcd1(long long n,long long m,long long &x,long long &y){  //n<mにしろ
+
+  if(m==0){
+  	x=0;
+  	y=1;
+  	return n;
+  }
+  
+  long long temp,re=gcd1(m,n%m,x,y);
+  temp=x;
+  x=-(n/m)*x+y;
+  y=temp;
+  cout<<n<<","<<m<<",\t"<<n/m<<","<<x<<endl;
+  return re;
+}
+
+long long gcd(long long n,long long m,long long &x,long long &y){
+	if(n<m)return gcd1(m,n,y,x);
+	else return gcd1(n,m,y,x);
+}
+	
+
+
+int main(){
+  long long n,m,x,y,re;
+  cin>>n>>m;
+  re=gcd(n,m,x,y);		//nx+my=gcd(n,m)
+  printf("%lld*%lld+(%lld*%lld)=%lld\n",n,x,m,y,re);
+  return 0;
+}
+	
