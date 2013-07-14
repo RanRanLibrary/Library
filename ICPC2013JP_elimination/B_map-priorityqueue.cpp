c804016f73e64748a32ee39c08221bf18383ace4
nvip <nvip62@gmail.com>
2013-07-14 11:07:39 +0900

    I want sample input!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ ICPC2013JP_elimination/B_map-priorityqueue.cpp
@@ -0,0 +1,63 @@
+#include<cstdio>
+#include<iostream>
+#include<map>
+#include<queue>
+
+using namespace std;
+
+int main(){
+  int M,T,P,R,i,m,t,p,j;
+  int counts[55],pena[55][11],timesum[55];
+ 
+  map<int,priority_queue<int> >::iterator it;
+
+  while(1){
+    scanf("%d %d %d %d",&M,&T,&P,&R);
+    if(M==0 && T==0 && P==0 && R==0)break;
+
+    memset(counts,0,sizeof(counts));
+    memset(pena,0,sizeof(pena));
+    memset(timesum,0,sizeof(timesum));
+    map<int,priority_queue<int> > time[11];
+
+    for(i=0;i<R;i++){
+      scanf("%d %d %d %d",&m,&t,&p,&j);
+      if(j){
+        pena[t][p]+=20;
+      }else{
+        counts[t]++;
+        timesum[t]+=pena[t][p]+m;
+        time[counts[t]][timesum[t]].push(t);
+      }
+    }
+
+    priority_queue<int> temp;
+    bool f=0,flg=0;
+    for(i=10;i>0;i--){
+      cout<<i<<endl;
+      for(it=time[i].begin();it!=time[i].end();it++){
+        if(f)printf(",");
+        temp=it->second;
+        flg=0;
+        while(!temp.empty()){
+          if(flg)printf("=");
+          if(counts[temp.top()]==i){
+            printf("%d",temp.top());
+            f=flg=1;
+          }
+          temp.pop();
+        }
+      }
+    }
+    flg=0;
+    for(i=10;i>0;i--){
+      if(counts[i]==0){
+        if(flg)putchar('=');
+        printf("%d",i);
+        flg=1;
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
