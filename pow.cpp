c348b7ab30dab2895e55db1941907d64fb2eceef
oyas <poteto83@gmail.com>
2016-04-26 22:33:07 +0900

    modpow()追加

--- pow.cpp
+++ pow.cpp
@@ -3,25 +3,40 @@
 
 using namespace std;
 
-long long _pow(int n,int m){    //n^m
+typedef long long int ll;
+const ll MOD = 1e9+7;
+
+ll _pow(int n,int m){    //n^m
     if(m==0) return 1;
-    long long tmp=_pow(n,m/2);
+    ll tmp=_pow(n,m/2);
     return tmp*tmp*(m%2?n:1);
 }
 
-long long pow(int n,unsigned int m,int q=1e9+7){    //n^m%q
+ll pow(int n,unsigned int m,int q=1e9+7){    //n^m%q
 	if(m==0) return 1;
-	long long tmp = pow(n,m/2,q);
+	ll tmp = pow(n,m/2,q);
 	return ((tmp*tmp)%q)*(m%2?n:1)%q;
 }
 
+// n^m%MOD を求める。O( log(m) )
+// mに負の数を指定すると逆元を求める
+ll modpow(ll n, int m){
+	ll ret=1;
+	if( m < 0 ) m = MOD-1 + m;
+	for(int i=1; i<=m; i<<=1){
+		if( m&i ) ret = ret * n % MOD;
+		n = n*n % MOD;
+	}
+	return ret;
+}
+
 int main(){
-    long long n,m,q;
+    ll n,m,q;
     while(scanf("%lld %lld %lld",&n,&m,&q)!=EOF){
         if(q){
-            cout<<pow(n,m,q)<<endl;
+            cout << pow(n,m,q) << endl;
         }else{
-            cout<<_pow(n,m)<<endl;
+            cout << modpow(n,m) << endl;
         }
     }
    
