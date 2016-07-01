3e5b9991d3f03944332f52d88afb1f2130221a0e
oyas <poteto83@gmail.com>
2016-07-01 13:27:54 +0900

    機能追加。Flow_minクラス追加。

--- flow.cpp
+++ flow.cpp
@@ -12,7 +12,7 @@ typedef vector<int> vc;
 
 const int INF = 0x3fffffff;
 
-// 最大流を求める(Dinic法。bfs()をなくすとFord-Fulkerson法)
+// 最大流を求める(Dinic法)
 class Flow{
 	public:
 		int N;	// 頂点数
@@ -21,13 +21,22 @@ class Flow{
 		vector<int> lv;	// Sからの距離
 
 		// コンストラクタ n:頂点数
-		Flow(int n): F(n,vc(n)), C(n,vc(n)), N(n), edge(n) {}
+		Flow(int n): N(n), F(n,vc(n)), C(n,vc(n)), edge(n) {}
 
-		// 辺を追加する
+		// f->tにコストcの辺を追加する(同じf->tに対して何度も追加することも可能)
 		void addEdge(int f, int t, int c){
-			F[f][t] = C[f][t] = c;
+			F[f][t] += c;
+			C[f][t] += c;
 			edge[f].push_back(t);
 			edge[t].push_back(f);
+		}
+
+		// 同じf->tペアに対してaddEdge()をたくさん実行した後に実行するとよい
+		void uniqueEdge(){
+			for(auto e: edge){
+				sort( e.begin(), e.end() );
+				e.erase( unique( e.begin(), e.end() ), e.end() );
+			}
 		}
 
 		// sからの最短距離を求める。これを実行するとDFSでは最短経路の中から探索する
@@ -47,41 +56,104 @@ class Flow{
 		}
 
 		// 増加パスをDFSで探す
-		int flow(int n, int c, int T){
+		int dfs(int n, int c, int T){
 			if( n == T ) return c;
 			bf[n] = true;
-			for(int v: edge[n]) if( F[n][v] && !bf[v] && lv[v] > lv[n] ){	// 最後の条件をなくすとbfs()は要らなくなる
-				int f = flow(v, min(c, F[n][v]), T);
+			for(int v: edge[n]) if( F[n][v] && !bf[v] && lv[v] > lv[n] ){
+				int f = dfs(v, min(c, F[n][v]), T);
 				F[n][v] -= f;
 				F[v][n] += f;
-				if( f > 0 ) return f;
+				if( f ) return f;
 			}
 			return 0;
 		}
 
 		// 最大流を求める
 		int maxFlow(int S, int T){
+			if( S == T ) return INF;
 			int ret = 0;
-			while( bfs(S,T) )	// bfs()なしの場合は、この行はいらない
-				for(int add; bf.assign(N,0), add = flow(0, INF, T);) ret += add;
+			while( bfs(S,T) ) for(int add; bf.assign(N,0), add = dfs(S, INF, T);) ret += add;
+			return ret;
+		}
+
+		// 辺に流れている流量を取得する
+		int flow(int f, int t){ return C[f][t] - F[f][t]; }
+
+		// 隣接する辺の流量の合計を取得する。n=S,T以外の場合は、出た値を1/2倍したものがその点の流量
+		int flow(int n){
+			int ret=0;
+			for(int v: edge[n]) ret += abs( flow(n,v) );
 			return ret;
 		}
 
 		// グラフを初期値に戻す
-		void clear(){ rep(i,N) rep(j,N) F[i][j] = C[i][j]; }
+		void reset(){ rep(i,N) rep(j,N) F[i][j] = C[i][j]; }
+};
+
+// 実装の短いフロー(Ford-Fulkerson法)
+class Flow_min{
+	public:
+		int N;	// 頂点数
+		vector<vc> F, C, edge;	// F:残余グラフ C:初期容量 edge:隣接する頂点
+		vc bf;	// 調べ終わったかどうか
+
+		// コンストラクタ n:頂点数
+		Flow_min(int n): N(n), F(n,vc(n)), C(n,vc(n)), edge(n) {}
+
+		// f->tにコストcの辺を追加する(同じf->tに対して何度も追加することは不可)
+		void addEdge(int f, int t, int c){
+			F[f][t] = C[f][t] = c;
+			edge[f].push_back(t);
+			edge[t].push_back(f);
+		}
+
+		// 増加パスをDFSで探す
+		int dfs(int n, int c, int T){
+			int f = c * (n==T);
+			bf[n] = 1;
+			for(int v: edge[n]) if( !f && F[n][v] && !bf[v] ){
+				f = dfs(v, min(c, F[n][v]), T);
+				F[n][v] -= f;
+				F[v][n] += f;
+			}
+			return f;
+		}
+
+		// 目一杯流す(S,Tは異なる点)
+		void maxFlow(int S, int T){
+			while( bf.assign(N,0), dfs(S, INF, T) ){}
+		}
+
+		// 辺に流れている流量を取得する
+		int flow(int f, int t){ return C[f][t] - F[f][t]; }
+
+		// 隣接する辺の流量の合計を取得する。n=S,T以外の場合は、出た値を1/2倍したものがその点の流量
+		int flow(int n){
+			int ret=0;
+			for(int v: edge[n]) ret += abs( C[n][v] - F[n][v] );
+			return ret;
+		}
+
+		// グラフを初期値に戻す
+		void reset(){ rep(i,N) rep(j,N) F[i][j] = C[i][j]; }
 };
 
 int main(){
 	int V, E;
 	cin >> V >> E;
+
 	Flow fl(V);
+//	Flow_min fm(V);
 	rep(i, E){
 		int u, v, c;
 		cin >> u >> v >> c;
 		fl.addEdge( u, v, c );
+//		fm.addEdge( u, v, c );
 	}
 
 	cout << fl.maxFlow(0, V-1) << endl;
+//	fm.maxFlow(0, V-1);
+//	cout << fm.flow(0) << endl;
 
 	return 0;
 }
