f04b4300b2b7435f47663975e9071298819f9235
oyas <poteto83@gmail.com>
2016-10-04 03:45:06 +0900

    遅延伝播セグメントツリーの確認コード追加。

--- segTree.cpp
+++ segTree.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-typedef unsigned long long int ll;
+typedef long long int ll;
 
 const int INF = 1e9+9;
 
@@ -74,7 +74,7 @@ public:
 	vector<NODE> data;
 	int N;
 	SegTree(int _N){
-		// _N <= (N = 2^k) を見つける
+		// _N <= (N = 2^k) を満たすNを見つける
 		N = 1;
 		while( N < _N ) N<<=1;
 		data.resize(N*2);
@@ -92,61 +92,6 @@ public:
 		if( r == -1 ) r = l+1;
 		return prop(l,r,0,0,N);
 	}
-
-/*
-	// セグメントツリーの更新
-	void update(int l, T v){
-		if( l >= N ) return;
-		int k = l + N;	// ノード番号
-		get(k);	//遅延分の伝播
-		data[k].update(v);
-		updateNode( k/2 );
-	}
-	// 葉から根へ向かって更新
-	void updateNode(int k){
-		if( k <= 0 ) return;	// 終了条件
-		data[k].merge( data[k*2].get(), data[k*2+1].get() );
-		updateNode(k/2);	// 親へ
-	}
-	// セグメントツリーの更新(lazy)
-	void add(int l, int r, T v, int a=0, int b=-1, int k=1){
-		if( b == -1 ) b = N;
-		// 伝播
-		propLazy(a, b, k);
-		// 更新
-		if( l <= a && b <= r ){
-			// 完全に範囲内の場合
-			propLazy(a, b, k, v);
-		}else if( l < b && a < r ){
-			// 中途半端に含まれる場合
-			int m = (a+b) / 2;
-			add(l,r,v, a, m, k*2);
-			add(l,r,v, m, b, k*2+1);
-			data[k].merge( data[k*2].get(), data[k*2+1].get() );
-		}
-	}
-
-	// 値を取得 [l,r)
-	T get(int l, int r=-1, int a=0, int b=-1, int k=1){
-		if( r == -1 ) r = l+1;
-		if( b == -1 ) b = N;
-		// 伝播
-		propLazy(a, b, k);
-		// 取得
-		NODE nd;
-		if( l <= a && b <= r ){
-			// 完全に範囲内の場合
-			return data[k].get();
-		}else if( l < b && a < r ){
-			// 中途半端に含まれる場合
-			int m = (a+b) / 2;
-			T vl = get(l,r, a, m, k*2);
-			T vr = get(l,r, m, b, k*2+1);
-			nd.merge( vl, vr );
-		}
-		return nd.get();
-	}
-*/
 };
 
 
@@ -154,6 +99,7 @@ public:
 // 確認
 
 #include <algorithm>
+#include <set>
 #define rep(i,n) for(int i=0; i<(n); i++)
 #define rrep(i,n) for(int i=(n)-1; i>=0; i--)
 #define all(X) (X).begin(),(X).end()
@@ -185,6 +131,7 @@ int main(){
 
 
 // arc026_C
+/*
 SegTree<ll,node<ll,(ll)1e12> > sg(100005);
 int main(){
 	int N, L;
@@ -209,6 +156,7 @@ int main(){
 
 	return 0;
 }
+*/
 
 
 // 手動で確認
@@ -234,3 +182,69 @@ int main(){
 }
 */
 
+
+// ICPC 2014 Tokyo Regional G
+/*
+SegTree<int> sg(300005);
+int N, Q;
+string s;
+int q[200005];
+set<int> op, cl;
+void flip(int i){
+	if(s[i] == '('){
+		s[i] = ')';
+		cl.insert( i );
+		if(op.find(i) != op.end() ) op.erase( op.find(i) );
+		sg.add(i+1, N+2, -2);
+	}else{
+		s[i] = '(';
+		op.insert( i );
+		if(cl.find(i) != cl.end() ) cl.erase( cl.find(i) );
+		sg.add(i+1, N+2, 2);
+	}
+}
+int main(){
+	cin >> N >> Q >> s;
+	rep(i,Q){
+		cin >> q[i];
+		q[i]--;
+	}
+
+	int sum = 0;
+	rep(i,N+1){
+		sg.update(i, sum);
+		if( s[i] == ')' ){
+			cl.insert(i);
+			sum--;
+		}else{
+			op.insert(i);
+			sum++;
+		}
+	}
+
+	rep(i,Q){
+		int ans;
+		flip(q[i]);
+		if( s[q[i]] == ')' ){
+			ans = *cl.begin();
+		}else{
+			int l=1, r=N+2;
+			while( r-l > 1 ){
+				int m = (l+r) / 2;
+				sg.add(m, N+2, -2);
+				if( sg.get(m, N+2) >= 0 ){
+					r = m;
+				}else{
+					l = m;
+				}
+				sg.add(m, N+2, 2);
+			}
+			ans = *op.upper_bound( l-1 );
+		}
+		flip(ans);
+		cout << (ans+1) << endl;
+	}
+
+	return 0;
+}
+*/
